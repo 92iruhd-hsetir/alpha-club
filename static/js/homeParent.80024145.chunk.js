f627b42fb77ebba028d903159794d237942150e2
92iruhd-hsetir <ritesh.dhuri92@gmail.com>
2022-03-20 02:33:58 +0530

    Updates

---
+++ static/js/homeParent.80024145.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkalpha_club=self.webpackChunkalpha_club||[]).push([[142],{86:function(e,n,t){t.r(n);var u=t(152),a=t(791),r=t(184),c=(0,a.lazy)((function(){return t.e(246).then(t.bind(t,103))})),h=(0,a.lazy)((function(){return t.e(177).then(t.bind(t,753))}));n.default=function(){var e=(0,a.useState)(""),n=(0,u.Z)(e,2),t=n[0],l=n[1];return"home"===t?(0,r.jsx)(h,{}):(0,r.jsx)(c,{enterHome:function(){l("home")}})}}}]);
+//# sourceMappingURL=homeParent.80024145.chunk.js.map
