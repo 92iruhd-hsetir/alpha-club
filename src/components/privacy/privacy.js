e6a5ba10fe2f67b5035b7ec1f143fe81833a17e7
Ritesh Dhuri <ritesh.dhuri92@gmail.com>
2022-03-19 23:42:01 +0530

    Add files via upload

---
+++ src/components/privacy/privacy.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+function Tnc() {
+    return (
+        <section className="static__wrap section-row">
+            <div className="main__container--fluid">
+                <div className="main__container">
+                    <div className="static__container">
+                        <h1 className="section-heading aos-init" data-aos="fade-up">
+                            <span>Privacy Policy </span>
+                        </h1>
+                        <div className="static__box aos-init" data-aos="fade-up">
+                            <h2 className="static__title"> Introduction </h2>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae minima sint velit similique! Maiores, dolore, rerum libero doloribus eaque eius error ea minima nihil blanditiis sint similique, non unde? Sequi!</p>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae minima sint velit similique! Maiores, dolore, rerum libero doloribus eaque eius error ea minima nihil blanditiis sint similique, non unde? Sequi!</p>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae minima sint velit similique! Maiores, dolore, rerum libero doloribus eaque eius error ea minima nihil blanditiis sint similique, non unde? Sequi!</p>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae minima sint velit similique! Maiores, dolore, rerum libero doloribus eaque eius error ea minima nihil blanditiis sint similique, non unde? Sequi!</p>
+                        </div>
+                        <div className="static__box aos-init" data-aos="fade-up">
+                            <h2 className="static__title"> Intellectual Property Rights </h2>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae minima sint velit similique! Maiores, dolore, rerum libero doloribus eaque eius error ea minima nihil blanditiis sint similique, non unde? Sequi!</p>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae minima sint velit similique! Maiores, dolore, rerum libero doloribus eaque eius error ea minima nihil blanditiis sint similique, non unde? Sequi!</p>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae minima sint velit similique! Maiores, dolore, rerum libero doloribus eaque eius error ea minima nihil blanditiis sint similique, non unde? Sequi!</p>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae minima sint velit similique! Maiores, dolore, rerum libero doloribus eaque eius error ea minima nihil blanditiis sint similique, non unde? Sequi!</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Tnc;
